72c58c8d5b83644f0fdbaa4781baa44cfc271492
lucarl07 <junitinho2007@gmail.com>
2026-04-01 18:07:04 -0300

    :tada: init (6): Sistema de login simples

---
+++ 06_login-simples/src/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const views = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default StyleSheet.flatten([views]);
